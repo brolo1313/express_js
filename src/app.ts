a474b24d2b41e3aa1a62fba13302b33d17bb4b6e
brolo1313 <brolo1341@gmail.com>
2022-03-26 14:36:56 +0200

    added logger seervice

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import express, { Express } from "express";
 import { userRouter } from "./users/users";
 import { Server } from "http";
+import { LoggerService } from './loger/loger.service';
+
 
 export class App {
   app: Express;
   server: Server;
   port: number;
+  logger : LoggerService;
 
-  constructor() {
+  constructor(logger: LoggerService) {
     this.app = express();
     this.port = 3000;
+    this.logger =logger;
   }
 
   useRoutes() {
@@ -19,6 +23,6 @@ export class App {
   public async init() {
     this.useRoutes();
     this.server = this.app.listen(this.port);
-    console.log('serv start on port' + this.port);
+    this.logger.log('serv start on port' + this.port);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
+import { LoggerService } from './loger/loger.service';
 import { App } from './app'
 
 
 async function bootsrap() {
-    const app = new App();
+    const app = new App(new LoggerService);
     await app.init();
 }
 
